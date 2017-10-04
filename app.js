8590591ce75c5851e4a255f226deec9306ec36be
amanda lee anderson <amandalee.anderson@gmail.com>
2017-10-04 15:04:13 -0500

    index.jade is working now, even though it's rendering a <testing> element heh

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ var path = require('path');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade'); 
 app.use(express.static('public'));
-  
+// set routes
+app.get('/', function(req, res) {
+   res.render('index');
+});  
 // Set server port
 app.listen(4000);
 console.log('server is running');
