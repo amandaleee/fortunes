1a7b66eef60d8a9ebdc294518faac92bc891b9f5
amanda lee anderson <amandalee.anderson@gmail.com>
2017-10-06 14:42:23 -0500

    updates

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ var path = require('path');
 
 // views as directory for all template files
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade'); 
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 app.use(express.static('public'));
 // set routes
-app.get('/', function(req, res) {
-   res.render('index');
-});  
+//app.get('/', function(req, res) {
+//   res.render('index');
+//});
+exports.index = function(req, res){
+res.render('index', { title: 'Page title' });};  
 // Set server port
 app.listen(4000);
 console.log('server is running');
